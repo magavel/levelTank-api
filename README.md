eab622440afd54b3400bb002d66e5c54c30da21a
feron <avel.feron@gmail.com>
2021-04-14 17:04:06 +0200

    fc

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# levelTank-api
+# Strapi application
+
+A quick description of your strapi application
---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '702575deeec94ab39264372ab265ed2d'),
+    },
+  },
+});
